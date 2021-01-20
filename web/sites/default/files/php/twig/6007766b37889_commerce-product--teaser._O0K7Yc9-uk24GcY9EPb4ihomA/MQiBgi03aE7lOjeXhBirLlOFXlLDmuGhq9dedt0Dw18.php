64477babee52f1bf999c488e83acd228f1180eb5
Ivan Peev <idobrinovv@gmail.com>
2021-01-20 02:55:33 +0200

    Add missed changes from previous commit

---
+++ web/sites/default/files/php/twig/6007766b37889_commerce-product--teaser._O0K7Yc9-uk24GcY9EPb4ihomA/MQiBgi03aE7lOjeXhBirLlOFXlLDmuGhq9dedt0Dw18.php
@@ -0,0 +1,124 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/contrib/estore/templates/commerce/commerce-product--teaser.html.twig */
+class __TwigTemplate_c9c751fcbc4007bbdef2aa72fef510c39c1dea14374624d88ac195a3f25bd56d extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 22
+        echo "
+<article";
+        // line 23
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => "commerce-product", 1 => "teaser"], "method", false, false, true, 23), 23, $this->source), "html", null, true);
+        echo ">
+  <div class=\"product-image\">
+    ";
+        // line 25
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed((($__internal_f607aeef2c31a95a7bf963452dff024ffaeb6aafbe4603f9ca3bec57be8633f4 = twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "field_images", [], "any", false, false, true, 25)) && is_array($__internal_f607aeef2c31a95a7bf963452dff024ffaeb6aafbe4603f9ca3bec57be8633f4) || $__internal_f607aeef2c31a95a7bf963452dff024ffaeb6aafbe4603f9ca3bec57be8633f4 instanceof ArrayAccess ? ($__internal_f607aeef2c31a95a7bf963452dff024ffaeb6aafbe4603f9ca3bec57be8633f4[0] ?? null) : null), 25, $this->source), "html", null, true);
+        echo "
+    <div class=\"products-hover-wrapper\"></div>
+  </div>
+
+  <h3 class=\"product-title\">
+    <a href=\"";
+        // line 30
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["product_url"] ?? null), 30, $this->source), "html", null, true);
+        echo "\">";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "title", [], "any", false, false, true, 30), 30, $this->source), "html", null, true);
+        echo "</a>
+  </h3>
+
+  ";
+        // line 33
+        if (((($__internal_62824350bc4502ee19dbc2e99fc6bdd3bd90e7d8dd6e72f42c35efd048542144 = (($__internal_1cfccaec8dd2e8578ccb026fbe7f2e7e29ac2ed5deb976639c5fc99a6ea8583b = twig_get_attribute($this->env, $this->source, ($context["product"] ?? null), "field_sale", [], "any", false, false, true, 33)) && is_array($__internal_1cfccaec8dd2e8578ccb026fbe7f2e7e29ac2ed5deb976639c5fc99a6ea8583b) || $__internal_1cfccaec8dd2e8578ccb026fbe7f2e7e29ac2ed5deb976639c5fc99a6ea8583b instanceof ArrayAccess ? ($__internal_1cfccaec8dd2e8578ccb026fbe7f2e7e29ac2ed5deb976639c5fc99a6ea8583b[0] ?? null) : null)) && is_array($__internal_62824350bc4502ee19dbc2e99fc6bdd3bd90e7d8dd6e72f42c35efd048542144) || $__internal_62824350bc4502ee19dbc2e99fc6bdd3bd90e7d8dd6e72f42c35efd048542144 instanceof ArrayAccess ? ($__internal_62824350bc4502ee19dbc2e99fc6bdd3bd90e7d8dd6e72f42c35efd048542144["#markup"] ?? null) : null) == 1)) {
+            // line 34
+            echo "    <div class=\"sales-tag\">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(t("Sale!"));
+            echo "</div>
+  ";
+        }
+        // line 37
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->withoutFilter($this->sandbox->ensureToStringAllowed(($context["product"] ?? null), 37, $this->source), "variation_attributes", "field_images", "title", "field_sale"), "html", null, true);
+        // line 43
+        echo "</article>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/contrib/estore/templates/commerce/commerce-product--teaser.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  73 => 43,  71 => 37,  65 => 34,  63 => 33,  55 => 30,  47 => 25,  42 => 23,  39 => 22,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/contrib/estore/templates/commerce/commerce-product--teaser.html.twig", "C:\\xampp\\htdocs\\FoodOrder\\web\\themes\\contrib\\estore\\templates\\commerce\\commerce-product--teaser.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("if" => 33);
+        static $filters = array("escape" => 23, "t" => 34, "without" => 37);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape', 't', 'without'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
